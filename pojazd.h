9caa723f7f446ebd18610f00598a2183daed8ff0
mats-kr <65515453+mats-kr@users.noreply.github.com>
2020-06-20 02:43:09 +0200

    Update pojazd.h

--- pojazd.h
+++ pojazd.h
@@ -1,10 +1,33 @@
-class pojazd
+#ifndef POJAZD_H
+#define POJAZD_H
+
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include<iostream>
+#include <string>
+#include <ctime>
+
+class Pojazd: public sf::RectangleShape
 {
+private:
+    sf::Vector2f speed;
+    bool is_selected_ = false;
+    int left_bound;
+    int right_bound;
+    int top_bound;
+    int bottom_bound;
+    sf::Clock time;
 public:
-    pojazd();
-    void move();
-    void setspeed();
-    void setilosc();
-    void setposition();
-    
+    Pojazd(const sf::Vector2f &position);
+    void Set_Bounds(const int& left,const int& right,const int& top,const int& bottom);
+    void Set_Speed(const int& speedY,const int& speedX);
+    void New_Position(const int& w);
+    void Size(int x,int y);
+    void Color(sf::Color(col));
+    void Animate();//const int& h,const int& w);
+    sf::FloatRect Give_Bounds();
+
 };
+
+#endif // POJAZD_H
