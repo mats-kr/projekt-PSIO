0d48ef4b19b52112160e9e8095f7b44c9cc5309e
mats-kr <65515453+mats-kr@users.noreply.github.com>
2020-06-20 02:40:25 +0200

    Update elementysceny.h

--- elementysceny.h
+++ elementysceny.h
@@ -1,9 +1,22 @@
-class elementysceny:public sf::Sprite
+#ifndef ELEMENTYSCENY_H
+#define ELEMENTYSCENY_H
+
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+class elementysceny:public sf::RectangleShape
 {
-    elementysceny();
-    void setroad();
-    void setstart();
-    void setfinish();
-    void setsafeplace();
-    void iscoliding();
+
+
+public:
+    elementysceny(const sf::Vector2f &position);
+    void Size(int x,int y);
+    void Color(sf::Color(col));
+    void Bounds(const int &left, const int& right, const int& top,const int& bottom);
+
+    sf::FloatRect Give_Bounds();
 };
+
+#endif // ELEMENTYSCENY_H
+
