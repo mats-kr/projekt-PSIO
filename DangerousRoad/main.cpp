11efbc2c953ff78cbc6177873ff7f9a0759dec77
mats-kr <65515453+mats-kr@users.noreply.github.com>
2020-07-17 23:08:47 +0200

    Update v11

---
+++ DangerousRoad/main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <memory.h>
+#include <ctime>
+#include"scene.h"
+
+int main()
+{
+    Scene scena;
+    scena.RenderGame();
+}
